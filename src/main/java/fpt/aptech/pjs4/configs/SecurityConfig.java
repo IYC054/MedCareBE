3762f55cdc077be98a1d76512ac46d768a7c8e4f
IYC <trieunhananh@gmail.com>
2024-11-12 23:13:04 +0700

    fix

--- src/main/java/fpt/aptech/pjs4/configs/SecurityConfig.java
+++ src/main/java/fpt/aptech/pjs4/configs/SecurityConfig.java
@@ -1,2 +1,23 @@
-package fpt.aptech.medcarebe.configs;
+package fpt.aptech.pjs4.configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public CorsFilter corsFilter(){
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
