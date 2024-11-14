4555b2203a544375fa72eeca53dac6cc23a27034
jortboy2 <vonghung849@gmail.com>
2024-11-14 14:58:47 +0700

    fix

--- src/main/java/fpt/aptech/pjs4/controllers/PatientFileController.java
+++ src/main/java/fpt/aptech/pjs4/controllers/PatientFileController.java
@@ -1,12 +1,16 @@
 package fpt.aptech.pjs4.controllers;
 
 import fpt.aptech.pjs4.entities.FilesImage;
+import fpt.aptech.pjs4.entities.Patient;
 import fpt.aptech.pjs4.entities.PatientFile;
 import fpt.aptech.pjs4.services.FilesImageService;
 import fpt.aptech.pjs4.services.PatientFileService;
+import fpt.aptech.pjs4.services.PatientService;
 import jakarta.servlet.ServletContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,42 +24,52 @@ import java.util.List;
 @RestController
 @RequestMapping("api/patientsfile")
 public class PatientFileController {
+    @Value("src/main/resources/static/image/")
+    private String fileUpload;
     @Autowired
     ServletContext servletContext;
+    private PatientFileService patientFilesService;
     @Autowired
-    private PatientFileService patientFileService;
+    private PatientService patientsService;
     @Autowired
-    private FilesImageService filesImageService;
+    private FilesImageService filesImagesService;
 
-    private final Path rootlocation = Paths.get("src\\main\\resources\\static\\uploads");
+
+    public PatientFileController(FilesImageService filesImagesService,PatientService patientsService,ServletContext servletContext, PatientFileService patientFilesService) {
+        this.filesImagesService = filesImagesService;
+        this.patientFilesService = patientFilesService;
+        this.servletContext = servletContext;
+        this.patientFilesService = patientFilesService;
+    }
+
     @PostMapping
-    public ResponseEntity<PatientFile> createPatientFile(@RequestParam("patientFile") PatientFile patientFile,
-                                                         @RequestParam("url_image") MultipartFile file) {
+    public ResponseEntity<PatientFile> createPatientFile(@ModelAttribute PatientFile patientFile,
+                                                         @RequestParam("patients_id") Integer patientsId,
+                                                         @RequestParam("url_image") List<MultipartFile> files) {
         try {
-            System.out.println();
-
-            String uploadRootPath = this.rootlocation.toAbsolutePath().toString();
-            File uploadDir = new File(uploadRootPath);
-
+            File uploadDir = new File(fileUpload);
             if (!uploadDir.exists()) {
                 uploadDir.mkdirs();
             }
+            Patient patient = patientsService.getPatientById(patientsId);
+            patientFile.setPatients(patient);
+            PatientFile createdPatientFile = patientFilesService.createPatientFile(patientFile);
 
-            String filename = file.getOriginalFilename();
-            File serverfile = new File(uploadDir.getAbsoluteFile() + File.separator + filename);
+            for (MultipartFile file : files) {
+                String fileName = file.getOriginalFilename();
+                File destinationFile = new File(uploadDir, fileName);
 
-            PatientFile createdPatientFile = patientFileService.createPatientFile(patientFile);
+                FileCopyUtils.copy(file.getBytes(), destinationFile);
 
-            uploadPatientFile(file);
+                FilesImage fileImage = new FilesImage();
+                fileImage.setUrlImage(fileName);
+                fileImage.setPatientsFiles(createdPatientFile);
 
-
-            FilesImage fileImage = new FilesImage();
-            PatientFile p = new PatientFile(createdPatientFile.getId());
-            fileImage.setUrlImage(serverfile.toString());
-            fileImage.setPatientsFiles(p);
-            filesImageService.createFilesImage(fileImage);
+                filesImagesService.createFilesImage(fileImage);
+            }
 
             return ResponseEntity.status(201).body(createdPatientFile);
+
         } catch (Exception e) {
             throw new RuntimeException("Error while creating patient file", e);
         }
@@ -63,49 +77,29 @@ public class PatientFileController {
 
     @GetMapping("/{id}")
     public ResponseEntity<PatientFile> getPatientFileById(@PathVariable int id) {
-        PatientFile patientFile = patientFileService.getPatientFileById(id);
+        PatientFile patientFile = patientFilesService.getPatientFileById(id);
         return ResponseEntity.ok(patientFile);
     }
 
     @GetMapping
     public ResponseEntity<List<PatientFile>> getAllPatientFiles() {
-        List<PatientFile> patientFilesList = patientFileService.getAllPatientFiles();
+        List<PatientFile> patientFilesList = patientFilesService.getAllPatientFiles();
         return ResponseEntity.ok(patientFilesList);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<PatientFile> updatePatientFile(@PathVariable int id, @RequestBody PatientFile patientFile) {
-        PatientFile updatedPatientFile = patientFileService.updatePatientFile(id, patientFile);
+        PatientFile updatedPatientFile = patientFilesService.updatePatientFile(id, patientFile);
         return ResponseEntity.ok(updatedPatientFile);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePatientFile(@PathVariable int id) {
-        FilesImage filesImage = filesImageService.getFilesImageById(id);
+        FilesImage filesImage = filesImagesService.getFilesImageById(id);
         File image = new File(filesImage.getUrlImage());
 
-        patientFileService.deletePatientFile(id);
+        patientFilesService.deletePatientFile(id);
         return ResponseEntity.noContent().build();
     }
-    public String uploadPatientFile(MultipartFile file) {
-//        String uploadRootPath = servletContext.getRealPath("upload");
-        String uploadRootPath = this.rootlocation.toAbsolutePath().toString();
-        File uploadDir = new File(uploadRootPath);
-        if(!uploadDir.exists()){
-            uploadDir.mkdirs();
-        }
-        try {
-            String filename = file.getOriginalFilename();
-            File serverfile = new File(uploadDir.getAbsoluteFile() + File.separator + filename);
-            BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverfile));
-            stream.write(file.getBytes());
-            stream.close();
-            System.out.println("#####" + serverfile);
 
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
-
-        return "Upload success";
-    }
 }
